a636ca5ca99e4071912642563c60a4122b20d7f4
Abrilpoggio <163590084+Abrilpoggio@users.noreply.github.com>
2026-03-31 02:43:44 -0300

    Agrego comentarios a primes.py

--- src/primes.py
+++ src/primes.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 # Python program to display all the prime numbers within an interval
+# Programa: primes.py
+# Descripción: muestra todos los números primos en un intervalo dado
+# Materia: Ingeniería de Software 2
+# Alumna: Abril Poggio
 
 lower = 1
 upper = 500
